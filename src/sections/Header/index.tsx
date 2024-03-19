6f99e250eab50a4472e8537151d9c66e0aac11cf
OI418710 <marcos.campos@oi.net.br>
2024-03-19 19:57:54 -0300

    Init Header | init Global css

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -2,7 +2,41 @@ import React from 'react';
 import styles from './style.module.scss';
 
 const Header = () => {
-  return <div className={styles.teste}>Header</div>;
+  return (
+    <header className={styles.header}>
+      <h2>MarcosCoelho</h2>
+      <a href='https://drive.google.com/file/d/1__BcaoPtUrONDdBu-mInk33NBYdWN_sq/view'>
+        CV
+      </a>
+      <button>
+        <img src='/public/icons/open-menu.svg' alt='Abrir menu' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#sobre'> Sobre </a>
+          </li>
+          <li>
+            <a href='#projetos'> Projetos </a>
+          </li>
+          <li>
+            <a href='#experiencia'> Experiencia </a>
+          </li>
+          <li>
+            <a href='#formacao'> Formação </a>
+          </li>
+          <li>
+            <a href='#contato'> Contato</a>
+          </li>
+          <li>
+            <a href='https://drive.google.com/file/d/1__BcaoPtUrONDdBu-mInk33NBYdWN_sq/view'>
+              CV
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
