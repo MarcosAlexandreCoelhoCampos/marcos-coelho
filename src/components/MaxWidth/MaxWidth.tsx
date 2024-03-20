14fe7a11387b7ff98b100111514e9593f8d7012a
OI418710 <marcos.campos@oi.net.br>
2024-03-20 13:59:03 -0300

    Finish Header

--- src/components/MaxWidth/MaxWidth.tsx
+++ src/components/MaxWidth/MaxWidth.tsx
@@ -1,12 +1,17 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styles from './style.module.scss';
 
 interface MaxWidthProps {
-  children: ReactNode;
+  style?: React.CSSProperties;
+  children: React.ReactNode;
 }
 
-const MaxWidth: React.FC<MaxWidthProps> = ({ children }) => {
-  return <div className={styles.MaxWidth}>{children}</div>;
+const MaxWidth: React.FC<MaxWidthProps> = ({ style = {}, children }) => {
+  return (
+    <div className={styles.MaxWidth} style={{ ...style }}>
+      {children}
+    </div>
+  );
 };
 
 export default MaxWidth;
--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -4,76 +4,81 @@ import MaxWidth from '../../components/MaxWidth/MaxWidth';
 
 const Header: React.FC = () => {
   const [menuNavActive, setmenuNavActive] = React.useState(false);
+  const menuMobileStyle = menuNavActive
+    ? {
+        borderBottom: '2px solid #ffffff',
+        paddingBottom: '6px',
+      }
+    : {};
 
   return (
     <header className={`${styles.header} ${menuNavActive && styles.active}`}>
-      <MaxWidth>
+      <MaxWidth style={menuMobileStyle}>
         <div className={styles.container}>
-          <div className={styles.containerMenu}>
-            <div className={styles.containerLogo}>
-              <h2 className='font-logo'>MarcosCoelho</h2>
-            </div>
-            <div className={styles.containerNav}>
-              <a
-                className={`font-nav ${styles.curriculo}`}
-                href='https://drive.google.com/file/d/1__BcaoPtUrONDdBu-mInk33NBYdWN_sq/view'
-              >
-                CV
-              </a>
-
-              <div className={`${styles.openCloseMenu}`}>
-                <button
-                  className={styles.openMenu}
-                  onClick={() => setmenuNavActive(true)}
-                >
-                  <img src='/public/icons/open-menu.svg' alt='Abrir menu' />
-                </button>
-                <button
-                  className={styles.closeMenu}
-                  onClick={() => setmenuNavActive(false)}
-                >
-                  <img src='/public/icons/close-menu.svg' alt='Fechar menu' />
-                </button>
-              </div>
-            </div>
+          <div className={styles.containerLogo}>
+            <h2 className='font-logo'>MarcosCoelho</h2>
           </div>
-          <nav className={`${styles.menuNav}`}>
-            <ul>
-              <li>
-                <a className='font-nav' href='#sobre'>
-                  Sobre
-                </a>
-              </li>
-              <li>
-                <a className='font-nav' href='#projetos'>
-                  Projetos
-                </a>
-              </li>
-              <li>
-                <a className='font-nav' href='#experiencia'>
-                  Experiencia
-                </a>
-              </li>
-              <li>
-                <a className='font-nav' href='#formacao'>
-                  Formação
-                </a>
-              </li>
-              <li>
-                <a className='font-nav' href='#contato'>
-                  Contato
-                </a>
-              </li>
-              <li>
-                <a
-                  className='font-nav'
-                  href='https://drive.google.com/file/d/1__BcaoPtUrONDdBu-mInk33NBYdWN_sq/view'
-                >
-                  CV
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <div className={styles.containerNav}>
+            <a
+              className={`font-nav ${styles.curriculo}`}
+              href='https://drive.google.com/file/d/1__BcaoPtUrONDdBu-mInk33NBYdWN_sq/view'
+            >
+              CV
+            </a>
+
+            <div className={`${styles.openCloseMenu}`}>
+              <button
+                className={styles.openMenu}
+                onClick={() => setmenuNavActive(true)}
+              >
+                <img src='/public/icons/open-menu.svg' alt='Abrir menu' />
+              </button>
+              <button
+                className={styles.closeMenu}
+                onClick={() => setmenuNavActive(false)}
+              >
+                <img src='/public/icons/close-menu.svg' alt='Fechar menu' />
+              </button>
+            </div>
+
+            <nav className={`${styles.menuNav}`}>
+              <ul>
+                <li>
+                  <a className='font-nav' href='#sobre'>
+                    Sobre
+                  </a>
+                </li>
+                <li>
+                  <a className='font-nav' href='#projetos'>
+                    Projetos
+                  </a>
+                </li>
+                <li>
+                  <a className='font-nav' href='#experiencia'>
+                    Experiencia
+                  </a>
+                </li>
+                <li>
+                  <a className='font-nav' href='#formacao'>
+                    Formação
+                  </a>
+                </li>
+                <li>
+                  <a className='font-nav' href='#contato'>
+                    Contato
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={`font-nav ${styles.curriculo}`}
+                    href='https://drive.google.com/file/d/1__BcaoPtUrONDdBu-mInk33NBYdWN_sq/view'
+                  >
+                    CV
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </MaxWidth>
     </header>
