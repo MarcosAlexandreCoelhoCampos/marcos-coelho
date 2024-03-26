4d945ff572fcc9d9eeb01c8775c8203056479fda
OI418710 <marcos.campos@oi.net.br>
2024-03-26 17:35:54 -0300

    Add classname on MaxWidth

--- src/components/MaxWidth/MaxWidth.tsx
+++ src/components/MaxWidth/MaxWidth.tsx
@@ -3,12 +3,13 @@ import styles from './style.module.scss';
 
 interface MaxWidthProps {
   style?: React.CSSProperties;
+  className?: string;
   children: React.ReactNode;
 }
 
-const MaxWidth: React.FC<MaxWidthProps> = ({ style = {}, children }) => {
+const MaxWidth: React.FC<MaxWidthProps> = ({ style, className, children }) => {
   return (
-    <div className={styles.MaxWidth} style={{ ...style }}>
+    <div className={`${styles.MaxWidth} ${className}`} style={{ ...style }}>
       {children}
     </div>
   );
