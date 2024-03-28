30aa0d2bcbb673caf466fa0427ce775dd1ec17b9
OI418710 <marcos.campos@oi.net.br>
2024-03-27 21:46:06 -0300

    Fix Bugs and Separete Banner and CreateStarsOnContainer

---
+++ src/components/CreateStarsOnContainer/index.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+interface CreateStarsOnContainerProps {
+  svgs: string[];
+  index: number;
+  imgHeight: number;
+  imgWidth: number;
+  containerRef: React.RefObject<HTMLDivElement>;
+  whiteSpaceRef: React.RefObject<HTMLHeadingElement>;
+}
+
+const CreateStarsOnContainer: React.FC<CreateStarsOnContainerProps> = ({
+  svgs,
+  index,
+  imgHeight,
+  imgWidth,
+  containerRef,
+  whiteSpaceRef,
+}) => {
+  const getRandomPosition = () => {
+    const boundingClientRect = containerRef.current?.getBoundingClientRect();
+    const maxX = boundingClientRect ? boundingClientRect.width - imgWidth : 0;
+    const maxY = boundingClientRect ? boundingClientRect.height - imgHeight : 0;
+
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+
+    return { x, y };
+  };
+
+  let allStartPosition: { top: number; left: number }[] = [];
+
+  const createStar = (index: number): JSX.Element | null => {
+    const { x, y } = getRandomPosition();
+
+    const mainTitleInfos = whiteSpaceRef.current?.getBoundingClientRect();
+
+    const positionAlreadyExists = allStartPosition.some(
+      (startPosition) =>
+        (startPosition.top === y && startPosition.left === x) ||
+        (startPosition.top - 25 < y &&
+          y < startPosition.top + 25 &&
+          startPosition.left - 25 < x &&
+          x < startPosition.left + 25)
+    );
+
+    if (mainTitleInfos) {
+      const { top, left, width, height } = mainTitleInfos;
+
+      const imgWidth = 30;
+      const imgHeight = 30;
+
+      if (
+        !(
+          x + imgWidth < left ||
+          x > left + width ||
+          y + imgHeight < top ||
+          y > top + height
+        )
+      ) {
+        return createStar(index);
+      }
+    }
+
+    if (positionAlreadyExists) {
+      return createStar(index);
+    }
+
+    allStartPosition.push({
+      top: y,
+      left: x,
+    });
+
+    return (
+      <img
+        src={`./icons/stars/white/${svgs[Math.floor(Math.random() * 4)]}`}
+        alt='Estrela'
+        key={index}
+        style={{
+          position: 'absolute',
+          top: y ? y : 20,
+          left: x ? x : 20,
+        }}
+      />
+    );
+  };
+
+  return <>{createStar(index)}</>;
+};
+
+export default CreateStarsOnContainer;
--- src/pages/Home/sections/Banner/index.tsx
+++ src/pages/Home/sections/Banner/index.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import styles from './style.module.scss';
+import CreateStarsOnContainer from '../../../../components/CreateStarsOnContainer';
 
-const Banner = () => {
-  const homeBannerRef = React.useRef(null);
-  const mainTitleRef = React.useRef(null);
-
-  const getRandomPosition = () => {
-    const imgWidth = 30;
-    const imgHeight = 30;
-
-    const maxX =
-      homeBannerRef?.current?.getBoundingClientRect().width - imgWidth;
-    const maxY =
-      homeBannerRef?.current?.getBoundingClientRect().height - imgHeight;
-
-    const x = Math.floor(Math.random() * maxX);
-    const y = Math.floor(Math.random() * maxY);
-
-    return { x, y };
-  };
+const Banner: React.FC = () => {
+  const homeBannerRef = React.useRef<HTMLDivElement>(null);
+  const mainTitleRef = React.useRef<HTMLHeadingElement>(null);
 
   const svgs = [
     'star-average-no-tip-white.svg',
@@ -26,65 +12,6 @@ const Banner = () => {
     'star-big-white.svg',
     'star-small-white.svg',
   ];
-
-  let allStartPosition = [];
-
-  const createStar = (index: number) => {
-    const { x, y } = getRandomPosition();
-
-    const mainTitleInfos = mainTitleRef?.current?.getBoundingClientRect();
-
-    const positionAlreadyExists = allStartPosition.some(
-      (startPosition) =>
-        (startPosition.top === y && startPosition.left === x) ||
-        (startPosition.top - 25 < y &&
-          y < startPosition.top + 25 &&
-          startPosition.left - 25 < x &&
-          x < startPosition.left + 25)
-    );
-
-    if (mainTitleInfos) {
-      const { top, left, width, height } = mainTitleInfos;
-
-      // Adicione a largura e a altura da imagem à sua verificação
-      const imgWidth = 30; // Substitua pelo valor real
-      const imgHeight = 30; // Substitua pelo valor real
-
-      if (
-        !(
-          x + imgWidth < left ||
-          x > left + width ||
-          y + imgHeight < top ||
-          y > top + height
-        )
-      ) {
-        return createStar(index);
-      }
-    }
-
-    if (positionAlreadyExists) {
-      return createStar(index);
-    }
-
-    allStartPosition.push({
-      top: y,
-      left: x,
-    });
-
-    return (
-      <img
-        src={`./icons/stars/white/${svgs[Math.floor(Math.random() * 4)]}`}
-        alt='Estrela'
-        key={index}
-        style={{
-          position: 'absolute',
-          top: y ? y : 20,
-          left: x ? x : 20,
-        }}
-      />
-    );
-  };
-
   return (
     <section id={styles.homeBanner} ref={homeBannerRef}>
       <h1
@@ -97,7 +24,17 @@ const Banner = () => {
           UX/UI Design`}
       </h1>
 
-      {Array.from({ length: 25 }).map((_, index) => createStar(index))}
+      {Array.from({ length: 25 }).map((_, index) => (
+        <CreateStarsOnContainer
+          key={index}
+          svgs={svgs}
+          index={index}
+          imgHeight={30}
+          imgWidth={30}
+          containerRef={homeBannerRef}
+          whiteSpaceRef={mainTitleRef}
+        />
+      ))}
     </section>
   );
 };
